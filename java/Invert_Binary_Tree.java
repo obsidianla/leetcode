931fd18adae44292592c0ac70165e9274b0ece88
Bowen Zhi <bzhi@usc.edu>
2016-07-18 13:27:47 -0700

    Update Invert_Binary_Tree.java

--- java/Invert_Binary_Tree.java
+++ java/Invert_Binary_Tree.java
@@ -1,13 +1,14 @@
 // non-recursive
- 
+// using queue, first inverse front.left and front.right, then inverse front.left.left and front.left.right
+// and inverse front.right.left and front.right.right, then got it!
  public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
         }
-        
+        // Queue is an interface and can not be instantiated.
         Queue<TreeNode> qu = new LinkedList<TreeNode>();
         qu.add(root);
-        
+        // using isEmpty() to test whether it is empty
         while(!qu.isEmpty()) {
             TreeNode front = qu.poll();
             if (front.left != null) 
