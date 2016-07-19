280543dd2db7804baac212ba4a6abc205b8c274f
Bowen Zhi <bzhi@usc.edu>
2016-07-19 10:57:22 -0700

    Create Number_of 1 bits.py

---
+++ python/Number_of 1 bits.py
@@ -0,0 +1,12 @@
+def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        count = 0
+        while(n != 0):
+            
+            n = n & (n-1)
+            count += 1
+        return count
