71713a7bd4d5d3f5c1ed584244b94bd3fa2ab7ff
Bowen Zhi <bzhi@usc.edu>
2016-07-21 00:24:31 -0700

    Update Palindrome Number.java

--- java/Palindrome Number.java
+++ java/Palindrome Number.java
@@ -1,3 +1,5 @@
+//Method 1, convert int to string, extra space
+
 public boolean isPalindrome(int x) {
         String s = new Integer(x).toString();
         
@@ -11,6 +13,32 @@ public boolean isPalindrome(int x) {
             }
             l++;
             r--;
+        }
+        return true;
+    }
+// Method 2, without extra space
+
+public boolean isPalindrome(int x) {
+        
+        if(x < 0) return false;
+        if(x>=0 && x<10) return true;
+        int num = 1;
+        int y = x;
+        while(y>=10) {
+            y = y/10;
+            num *=10;
+        }
+        
+        while (x != 0) {
+            int left = x /num;
+            int right = x%10;
+            if(left != right) {
+                return false;
+            }
+            x = (x%num)/10;
+            
+            num = num/100;
+            
         }
         return true;
     }
