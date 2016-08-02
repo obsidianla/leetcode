a5290c60ef9d889f002eb6673efe8ac76b16b501
Bowen Zhi <bzhi@usc.edu>
2016-08-01 23:41:07 -0700

    Create Reverse Bits.cpp

---
+++ C_plus/Reverse Bits.cpp
@@ -0,0 +1,18 @@
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        int ans = 0;
+        
+        for(int i =0; i<32;i++){
+            
+            ans = ans<<1;
+            
+            ans = ans|(n & 1);
+            
+            n = n>>1;
+            
+        }
+        
+        return ans;
+        
+    }
