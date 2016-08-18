66027473c6f315a45eb959479a5ea64a9ccad929
Bowen Zhi <bzhi@usc.edu>
2016-08-18 10:43:59 -0700

    Update Sliding Window Maximum.py

--- python/Sliding Window Maximum.py
+++ python/Sliding Window Maximum.py
@@ -1,3 +1,5 @@
+# method 1, naive, scan the list, find the max for each window
+
 def maxSlidingWindow(self, nums, k):
         """
         :type nums: List[int]
@@ -11,4 +13,31 @@ def maxSlidingWindow(self, nums, k):
         
         for i in range(len(nums)+1-k):
             res.append(max(nums[i:i+k]))
+        return res
+        
+# method 2, use deque
+
+def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
+        res = []
+        
+        dq = collections.deque()
+        
+        for i in range(len(nums)):
+            #push the index max of sublist(k) into dq
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
+            dq.append(i)
+            
+            # pop the left number since slide the window
+            if dq[0] == i -k:
+                dq.popleft()
+            # append the max to the result
+            if i>=k-1:
+                res.append(nums[dq[0]])
         return res
